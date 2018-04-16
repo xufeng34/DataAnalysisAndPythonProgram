b5978cf4cfa268506d325c64dac6b14d3949a369
xufeng34 <xufeng34@163.com>
2018-04-16 17:31:40 +0800

    Delete 系统初始化.py

--- 系统初始化.py
+++
@@ -1,7 +0,0 @@
-from IPython.core.interactiveshell import InteractiveShell 
-InteractiveShell.ast_node_interactivity = “all” #多项输出
-
-import numpy as np #数值分析包 
-np.set_printoptions(precision=4) #数组输出4位小数 
-import pandas as pd #数据分析包 
-import matplotlib.pyplot as plt #基本绘图包
